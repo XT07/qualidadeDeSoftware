636d8098c1b0d7089840c7c3655fc7748325ea0d
Luiz <luizfelipe01700@gmail.com>
2025-03-06 01:39:56 -0300

    base atualizada v1.1

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # qualidadeDeSoftware
 Projeto de CRUD da aula de qualidade de software
+
+## breve instrução
+Qual quer informação que não esteja aqui vai estar documentado no própio código ou me pergunte
+
+Caso alguma pasta e ou arquivo não esteja criada pergunte a equipe se alguem já criou ela e como ele a nomeou ou apenas olhe no cmmit. Se não achar pergunte
+
+O comando "app.set("view engine", "ejs");" seta o ejs como o interpretador de html ou seja ao invés de por .html agora é .ejs, todos os arquivos .ejs e .css devem ficar dentro da pasta public ou no nome da pasta q foi setada pode criar subpastas para separar melhor desde q o front esteja dentro da pasta setada
+
+O comando "app.use(express.static("public"));" seta a pasta sitada acima
+
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+//nesse espaço faça a autenticação do DB
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(session({
+    secret: "pensandoEmAlgo",//secret server para proteger a session e não ficar visivel para o publico
+    cookie:{
+        maxAge: 2592000000//define o tempo q as informações do usuario ficam salvas no sistema (oq não estiver no DB)
+    }
+}))
+
+///aqui colocaremos as rotas sem relação entre si no sentido de que não tem uma certa "administração" em cima delas
+app.get("/", (req, res) => {
+    res.send("Aguardando a criação da página principal");
+})
+
+app.listen(3030, () => {
+    console.log("server on");
+})
