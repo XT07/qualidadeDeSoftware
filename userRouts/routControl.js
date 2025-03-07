02f73ffc86559bf9b99e31c9ef67f8127e5dc84e
LUIZ FELIPE RADOLL CORDEIRO <luiz.cordeiro2@alunos.sc.senac.br>
2025-03-06 21:11:20 -0300

    criei as rotas para a página do usuário rotas de criação de usuario, edição e deletar

---
+++ userRouts/routControl.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/user", (req, res) => {
+    //em andamento
+});
+
+router.post("/user", (req, res) => {
+    //em andamento
+});
+
+router.put("/user", (req, res) => {
+    //em andamento
+});
+
+router.delete("/user", (req, res) => {
+    //em andamento
+});
