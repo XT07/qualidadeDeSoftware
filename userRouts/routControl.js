857f15c2464c6ad5ceabb6e640036e946548fdf1
Luiz <luizfelipe01700@gmail.com>
2025-03-25 14:06:34 -0300

    rotas finalizadas - falta apenas testar o midleware e o envio dos codigos de recuperação

--- userRouts/routControl.js
+++ userRouts/routControl.js
@@ -3,20 +3,19 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const users = require("../modules/Users");
 
-router.get("/user", (req, res) => {
-    let id = req.session.user.id;
+const salt = bcrypt.genSaltSync(10);
 
-    users.findOne({ where: {id:id} }).then(user => {
-        res.sendStatus(200);
+router.get("/user", (req, res) => {
+    users.findOne({ where: {id: req.session.user.id} }).then(user => {
         res.render("perfil", {
             user: user
         });
-    })
+    });
 });
 
 router.get("/recovery", (req, res) => {
     res.render("senha");
-})
+});
 
 router.get("/registerUser", (req, res) => {
     res.render("cadastro");
@@ -27,9 +26,8 @@ router.post("/loginUser", (req, res) => {
     let pass = req.body.password;
 
     users.findOne({ where: { cpf:cpf } }).then(user => {
+        let verific = bcrypt.compareSync(pass, user.password);
 
-        let verific = bcrypt.compare(pass, user.password);
-        
         if(verific){
             req.session.user = {
                 id: user.id,
@@ -41,9 +39,8 @@ router.post("/loginUser", (req, res) => {
                 phone: user.phone,
                 recovery_email: user.recovery_email
             }
-
-            res.sendStatus(200);
             res.redirect("/home");
+            console.log(req.session.user.id);
         }else{
             res.sendStatus(401);
             console.log(`Senha não encontrado no nosso sistema por favor tente novamente`);
@@ -56,35 +53,37 @@ router.post("/loginUser", (req, res) => {
 
 });
 
-router.post("/user", (req, res) => {
+router.post("/user", async (req, res) => {
     let cpf = parseInt(req.body.cpf);
     let { name, password, email, birth_date, phone, recovery_email } = req.body;
+    let hash = await bcrypt.hashSync(password, salt);
 
     users.create({
         name: name,
         email: email,
         cpf: cpf,
-        pass: password,
+        password: hash,
         birth_date: birth_date,
         phone: phone,
         recovery_email: recovery_email
     }).then(() => {
-        res.redirect("/home");
-        res.sendStatus(200);
+        res.redirect("/");
     }).catch(err => {
         console.log(`Não foi possivel criar o usuário || ERR ${err}`);
         res.sendStatus(400);
     })
 });
 
-router.patch("/user", (req, res) => {
+router.patch("/user", async (req, res) => {
     let id = req.session.id;
     let cpf = parseInt(req.body.cpf);
     let { name, password, email, birth_date, phone, recovery_email } = req.body;
 
+    let hash = await bcrypt.hashSync(password, salt)
+
     let subData = {
         name: name,
-        password: password,
+        password: hash,
         email: email,
         birth_date: birth_date,
         cpf: cpf,
@@ -93,7 +92,6 @@ router.patch("/user", (req, res) => {
     }
 
     users.update(subData, { where: { id:id } }).then(() => {
-        res.sendStatus(200);
         res.redirect("/user");
     }).catch(err => {
         res.sendStatus(400);
@@ -106,13 +104,12 @@ router.delete("/user", async (req, res) => {
     let verific = await users.findOne({ where: {id:id} });
 
     if(verific){
-        users.destroy({ where: {id:id} }).then(user => {
-            res.statusCode(200);
+        users.destroy({ where: {id:id} }).then(() => {
             res.redirect("/");
         }).catch(err => {
             res.sendStatus(400);
             console.log(`ID não encontrado no nosso banco de dados | ${err}`);
-        })
+        });
     }else{
         res.sendStatus(400);
         console.log(`ID não encontrado no nosso banco de dados`);
